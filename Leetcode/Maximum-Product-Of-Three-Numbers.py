1cc4079caa5cad5104aacc6e61c92fafa98ed12f
patrix726 <a0911991679@gmail.com>
2025-02-05 15:21:16 +0300

    Maximum Product of Three Numbers

---
+++ Leetcode/Maximum-Product-Of-Three-Numbers.py
@@ -0,0 +1,24 @@
+from heapq import heapify, heappush, heappop, heappushpop
+
+
+class Solution:
+    def maximumProduct(self, nums: list[int]) -> int:
+        if len(nums) == 3:
+            return nums[0] * nums[1] * nums[2]
+        maxNums = nums[:3]
+        minNums = [-1 * num for num in nums[:3]]
+        for num in nums[3:]:
+            heapify(maxNums)
+            heapify(minNums)
+            heappushpop(maxNums, num)
+            heappushpop(minNums, -1 * num)
+        maxNum = max(maxNums)
+        postiveProd = heappop(maxNums) * heappop(maxNums) * heappop(maxNums)
+        heappop(minNums)
+        negativeProd = heappop(minNums) * heappop(minNums) * maxNum
+
+        return max(postiveProd, negativeProd)
+
+
+ans = Solution()
+print(ans.maximumProduct([-1, -2, -3]))
