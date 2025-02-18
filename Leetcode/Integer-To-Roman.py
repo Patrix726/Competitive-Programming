365dfa7ea13e865e1153562bd960c93995a8a2be
patrix726 <a0911991679@gmail.com>
2025-02-18 17:25:41 +0300

    Refactored integer-to-roman based on other solution

--- Leetcode/Integer-To-Roman.py
+++ Leetcode/Integer-To-Roman.py
@@ -1,41 +1,27 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
+        roman_to_int = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ]
         output = []
-        output.extend(["M" for i in range(num//1000)])
-        num = num%1000
-        if num>=900:
-            output.append("CM")
-            num %=900
-        elif num>=500:
-            output.append("D")
-            num %=500
-        elif num >= 400:
-            output.append("CD")
-            num %=400
-        output.extend(["C" for i in range(num//100)])
-        num%=100
-        
-        if num>=90:
-            output.append("XC")
-            num %=90
-        elif num>=50:
-            output.append("L")
-            num %=50
-        elif num>=40:
-            output.append("XL")
-            num %=40
-        output.extend(["X" for i in range(num//10)])
-        num%=10
-        if num==9:
-            output.append("IX")
-        elif num>=5:
-            output.append("V")
-            output.extend(["I" for i in range(num%5)])
-        elif num==4:
-            output.append("IV")
-        else:
-            output.extend(["I" for i in range(num)])
+        for level, symbol in roman_to_int:
+            while num >= level:
+                output.append(symbol)
+                num -= level
         return "".join(output)
-        
 
 
+ans = Solution()
+print(ans.intToRoman(58))
