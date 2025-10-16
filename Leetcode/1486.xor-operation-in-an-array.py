3cb9b05646bee11fe8969b77cb368dddafd3bc65
patrix726 <a0911991679@gmail.com>
2025-10-16 13:51:40 +0300

    1486. Xor Operation In An Array

---
+++ Leetcode/1486.xor-operation-in-an-array.py
@@ -0,0 +1,53 @@
+# @leet imports start
+import bisect
+import collections
+import copy
+import datetime
+import functools
+import heapq
+import io
+import itertools
+import json
+import math
+import operator
+import random
+import re
+import statistics
+import string
+import sys
+from bisect import *
+from builtins import *
+from collections import *
+from copy import *
+from datetime import *
+from functools import *
+from heapq import *
+from io import *
+from itertools import *
+from json import *
+from math import *
+from operator import *
+from random import *
+from re import *
+from statistics import *
+from string import *
+from sys import *
+from typing import *
+
+# @leet imports end
+
+
+# @leet start
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        nums = []
+        for i in range(n):
+            nums.append(start + 2 * i)
+        output = 0
+        for num in nums:
+            output ^= num
+        return output
+
+
+# @leet end
+
