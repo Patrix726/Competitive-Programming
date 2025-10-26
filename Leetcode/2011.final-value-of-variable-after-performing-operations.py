0b554c242c4136ec247ca6362c7932269088f7bd
patrix726 <a0911991679@gmail.com>
2025-10-26 12:21:34 +0300

    2011. Final Value Of Variable After Performing Operations

---
+++ Leetcode/2011.final-value-of-variable-after-performing-operations.py
@@ -0,0 +1,53 @@
+# @leet imports start
+import bisect
+import collections
+import copy
+import datetime
+import functools
+import heapq
+import io
+import itertools
+import json
+import math
+import operator
+import random
+import re
+import statistics
+import string
+import sys
+from bisect import *
+from builtins import *
+from collections import *
+from copy import *
+from datetime import *
+from functools import *
+from heapq import *
+from io import *
+from itertools import *
+from json import *
+from math import *
+from operator import *
+from random import *
+from re import *
+from statistics import *
+from string import *
+from sys import *
+from typing import *
+
+# @leet imports end
+
+
+# @leet start
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        operators = {
+            "++X": 1,
+            "X++": 1,
+            "X--": -1,
+            "--X": -1,
+        }
+        return sum([operators[i] for i in operations])
+
+
+# @leet end
+
