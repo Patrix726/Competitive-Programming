b4885455df8cd5a301378edd8ee98a8ff54fd244
patrix726 <a0911991679@gmail.com>
2025-10-26 12:18:21 +0300

    2475. Number Of Unequal Triplets In Array

---
+++ Leetcode/2475.number-of-unequal-triplets-in-array.py
@@ -0,0 +1,54 @@
+# @leet imports start
+import bisect
+import collections
+import copy
+import datetime
+import functools
+import heapq
+import io
+import itertools
+import json
+import math
+import operator
+import random
+import re
+import statistics
+import string
+import sys
+from bisect import *
+from builtins import *
+from collections import *
+from copy import *
+from datetime import *
+from functools import *
+from heapq import *
+from io import *
+from itertools import *
+from json import *
+from math import *
+from operator import *
+from random import *
+from re import *
+from statistics import *
+from string import *
+from sys import *
+from typing import *
+
+# @leet imports end
+
+
+# @leet start
+class Solution:
+    def unequalTriplets(self, nums: List[int]) -> int:
+        count = 0
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                for k in range(j + 1, n):
+                    if nums[i] != nums[j] and nums[i] != nums[k] and nums[j] != nums[k]:
+                        count += 1
+        return count
+
+
+# @leet end
+
