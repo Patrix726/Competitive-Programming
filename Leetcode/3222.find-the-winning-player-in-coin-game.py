6a4b38b1b1b11a2b1d3530be1148178a924d8b27
patrix726 <a0911991679@gmail.com>
2025-10-26 11:47:34 +0300

    3222. Find The Winning Player In Coin Game

---
+++ Leetcode/3222.find-the-winning-player-in-coin-game.py
@@ -0,0 +1,50 @@
+# @leet imports start
+import bisect
+import collections
+import copy
+import datetime
+import functools
+import heapq
+import io
+import itertools
+import json
+import math
+import operator
+import random
+import re
+import statistics
+import string
+import sys
+from bisect import *
+from builtins import *
+from collections import *
+from copy import *
+from datetime import *
+from functools import *
+from heapq import *
+from io import *
+from itertools import *
+from json import *
+from math import *
+from operator import *
+from random import *
+from re import *
+from statistics import *
+from string import *
+from sys import *
+from typing import *
+
+# @leet imports end
+
+
+# @leet start
+class Solution:
+    def winningPlayer(self, x: int, y: int) -> str:
+        players = ["Alice", "Bob"]
+        winner = min(x, y // 4) + 1
+
+        return players[winner % 2]
+
+
+# @leet end
+
