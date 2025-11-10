a36dd4af9e96bf03c6b83475f13a9b54b2eb5f36
Nebil <a0911991679@gmail.com>
2025-11-10 09:33:06 +0300

    278. First Bad Version

---
+++ Leetcode/278.first-bad-version.py
@@ -0,0 +1,61 @@
+# @leet imports start
+import bisect
+import collections
+import copy
+import datetime
+import functools
+import heapq
+import io
+import itertools
+import json
+import math
+import operator
+import random
+import re
+import statistics
+import string
+import sys
+from bisect import *
+from builtins import *
+from collections import *
+from copy import *
+from datetime import *
+from functools import *
+from heapq import *
+from io import *
+from itertools import *
+from json import *
+from math import *
+from operator import *
+from random import *
+from re import *
+from statistics import *
+from string import *
+from sys import *
+from typing import *
+
+# @leet imports end
+
+# @leet start
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 0
+        right = n
+
+        first_bad = n
+        while left <= right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                first_bad = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return first_bad
+
+
+# @leet end
